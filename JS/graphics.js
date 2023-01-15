fd1014ed5d3204abb2a9a33e8583106a7506a91c
DuckyProgramming <93790036+DuckyProgramming@users.noreply.github.com>
2023-01-15 10:40:07 -0500

    Standard Graphics Package

--- JS/graphics.js
+++ JS/graphics.js
@@ -10,7 +10,7 @@ function setupGraphics(){
 		graphics.backgrounds.push(createGraphics(900,600))
 		setupLayer(graphics.backgrounds[a])
 	}
-	graphics.minor=[160]
+	graphics.minor=[160,160]
 	for(let a=0,la=graphics.minor.length;a<la;a++){
 		graphics.minor[a]=createGraphics(graphics.minor[a],graphics.minor[a])
 		setupLayer(graphics.minor[a])
@@ -52,4 +52,41 @@ function setupGraphics(){
     }
     graphics.minor[0].fill(254,228,232)
     graphics.minor[0].ellipse(0,0,12,12)
+    graphics.minor[1].translate(80,80)
+    graphics.minor[1].strokeWeight(0.6)
+    graphics.minor[1].strokeJoin(ROUND)
+    for(a=0,la=100;a<la;a++){
+        for(b=0;b<5;b++){
+            graphics.minor[1].fill(136+59*a/la,61+7*a/la,92-5*a/la)
+            graphics.minor[1].stroke(136+59*a/la,61+7*a/la,92-5*a/la)
+            if(a<la/2){
+                graphics.minor[1].beginShape()
+                graphics.minor[1].vertex(0,0)
+                graphics.minor[1].bezierVertex(-21*(1-a/la*2),-20,-28*(1-a/la*2),-50,-3,-70)
+                graphics.minor[1].vertex(-7*(1-(a+1)/la*2),-64)
+                graphics.minor[1].endShape()
+            }
+            graphics.minor[1].rotate(-72)
+            graphics.minor[1].beginShape()
+            graphics.minor[1].vertex(0,0)
+            graphics.minor[1].bezierVertex(21,-20,28,-50,3,-70)
+            if(a>=la/2){
+                graphics.minor[1].vertex(7*(-1+a/la*2),-64)
+                graphics.minor[1].bezierVertex(7*(-1+a/la*2),-50,28*(-1+a/la*2),-14,0,0)
+            }else{
+                graphics.minor[1].vertex(0,-64)
+            }
+            graphics.minor[1].endShape(CLOSE)
+        }
+    }
+    for(a=0;a<5;a++){
+        graphics.minor[1].rotate(72)
+        graphics.minor[1].rotate(-12)
+        graphics.minor[1].noStroke()
+        graphics.minor[1].fill(124,41,51)
+        graphics.minor[1].quad(0,-4,3,-16,0,-24,-3,-16)
+        graphics.minor[1].rotate(12)
+    }
+    graphics.minor[1].fill(211,153,120)
+    graphics.minor[1].ellipse(0,0,12,12)
 }
