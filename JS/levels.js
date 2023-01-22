71159f377b5ba90865be00df6cb87a5ee5370419
DuckyProgramming <93790036+DuckyProgramming@users.noreply.github.com>
2023-01-21 22:40:45 -0500

    Level3

--- JS/levels.js
+++ JS/levels.js
@@ -1,25 +1,43 @@
 levels=[
 	{
-		edge:{x:750,y:450},
-		player:[240,240],
+		edge:{x:750,y:600},
+		player:[480,480],
 		walls:[
-			{position:{x:0,y:360},type:1,width:150,height:90},
-			{position:{x:150,y:330},type:1,width:150,height:120},
-			{position:{x:300,y:360},type:1,width:150,height:90},
-			{position:{x:450,y:390},type:1,width:150,height:60},
-			{position:{x:600,y:360},type:1,width:150,height:90},
+			{position:{x:0,y:510},type:1,width:150,height:90},
+			{position:{x:150,y:480},type:1,width:150,height:120},
+			{position:{x:300,y:510},type:1,width:150,height:90},
+			{position:{x:450,y:540},type:1,width:150,height:60},
+			{position:{x:600,y:510},type:1,width:150,height:90},
 		],
 	},{
-		edge:{x:900,y:450},
-		player:[240,240],
+		edge:{x:900,y:600},
+		player:[480,390],
 		walls:[
-			{position:{x:0,y:360},type:1,width:120,height:90},
-			{position:{x:150,y:300},type:1,width:120,height:150},
-			{position:{x:330,y:330},type:1,width:120,height:120},
-			{position:{x:480,y:210},type:1,width:90,height:240},
-			{position:{x:570,y:150},type:1,width:90,height:300},
-			{position:{x:660,y:210},type:1,width:90,height:240},
-			{position:{x:750,y:270},type:1,width:150,height:180},
+			{position:{x:0,y:510},type:1,width:120,height:90},
+			{position:{x:150,y:450},type:1,width:120,height:150},
+			{position:{x:330,y:480},type:1,width:120,height:120},
+			{position:{x:480,y:360},type:1,width:90,height:240},
+			{position:{x:570,y:300},type:1,width:90,height:300},
+			{position:{x:660,y:360},type:1,width:90,height:240},
+			{position:{x:750,y:420},type:1,width:150,height:180},
+		],
+	},{
+		edge:{x:900,y:600},
+		player:[510,210],
+		walls:[
+			{position:{x:0,y:540},type:1,width:90,height:60},
+			{position:{x:90,y:300},type:1,width:90,height:300},
+			{position:{x:240,y:0},type:1,width:90,height:420},
+			{position:{x:180,y:540},type:1,width:210,height:60},
+			{position:{x:480,y:300},type:1,width:90,height:300},
+			{position:{x:780,y:240},type:1,width:120,height:360},
 		],
 	},
+	/*{
+		edge:{x:0,y:600},
+		player:[0,0],
+		walls:[
+			{position:{x:0,y:0},type:1,width:0,height:0},
+		],
+	},*/
 ]
